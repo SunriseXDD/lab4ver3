98ee0e2e65163739157fbf8cff1b1d60089a4922
SunriseXDD <mikkyrock465@gmail.com>
2025-06-17 20:55:57 +0700

    Завершена глава 1. Назначены слушатели для тру и фолс баттон

--- app/src/main/java/com/example/lab4ver3/MainActivity.kt
+++ app/src/main/java/com/example/lab4ver3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab4ver3
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
@@ -14,8 +15,20 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+
         trueButton.setOnClickListener { view: View ->
-// Что-то выполнить после нажатия
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
         }
     }
 }
